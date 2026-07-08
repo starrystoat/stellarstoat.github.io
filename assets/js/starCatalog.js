8ff847f73b524bc040b6cf5851b7e7775d709ba4
StellarStoat <staristoat@gmail.com>
2026-07-08 11:32:21 -0700

    Create starCatalog.js

---
+++ assets/js/starCatalog.js
@@ -0,0 +1,26 @@
+/*
+==================================================
+ Stellar Stoat Portfolio
+ Version 2.3
+
+ starCatalog.js
+
+ SSoT for every project displayed on the website.
+==================================================
+*/
+
+const starCatalog = {
+
+    stellarStoat: {
+        id: "stellarStoat",
+        title: "Stellar Stoat",
+        category: "Programming",
+        featured: true,
+        thumbnail: "assets/images/projects/stellarStoat.jpg",
+        summary: "This portfolio website.",
+        description: `
+            Long description goes here.
+        `
+    }
+
+};
